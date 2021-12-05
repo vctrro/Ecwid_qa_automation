fc2429c6f46569850ef89542a012427ac8204c48
Victor Rodionov <v.a.rodionov@gmail.com>
2021-12-05 22:54:27 +0400

    Created SerchPageObject

---
+++ WebAuthorizationTest/PageObjects/SearchPageObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace WebAuthorizationTest.PageObjects
+{
+    public class SearchPageObject
+    {
+        private IWebDriver _webDriver;
+
+        private readonly By _searchInput = By.XPath("//input[@name='keyword']");
+
+        public SearchPageObject(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        public void SearchText(string text)
+        {
+            _webDriver.FindElement(_searchInput).SendKeys(text);
+        }
+
+
+    }
+}
--- WebAuthorizationTest/UnitTest1.cs
+++ WebAuthorizationTest/UnitTest1.cs
@@ -1,35 +1,27 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
+using WebAuthorizationTest.PageObjects;
 
 namespace WebAuthorizationTest
 {
+
     public class Tests
     {
         private IWebDriver driver;
-
-        private readonly By _singInButton = By.XPath("//span[text()='Войти'");
-        private readonly By _telInput = By.XPath("//input[@name='tel']");
-        private readonly By _emailInput = By.XPath("//input[@name='username']");
-
+        private SearchPageObject searchPageObject;
 
         [SetUp]
         public void Setup()
         {
             driver = new OpenQA.Selenium.Chrome.ChromeDriver();
-            driver.Navigate().GoToUrl("https://cian.ru");
+            driver.Navigate().GoToUrl("https://buy-in-10-seconds.company.site/search");
+            searchPageObject = new SearchPageObject(driver);
         }
 
         [Test]
         public void Test1()
         {
-            var singIn = driver.FindElement(_singInButton);
-            singIn.Click();
-
-            var tel = driver.FindElement(_telInput);
-            tel.SendKeys("7937345678");
-
-            var email = driver.FindElement(_emailInput);
-            email.SendKeys("test@test.ru");
+            searchPageObject.SearchText("Тов");
 
             //Assert.Pass();
 
