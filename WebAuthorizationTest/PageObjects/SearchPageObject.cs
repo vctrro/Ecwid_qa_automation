972e25a1be4e97a91968fd78ef0263d7420ab1bd
Victor Rodionov <v.a.rodionov@gmail.com>
2021-12-07 11:38:32 +0400

    Added waiting for elements load

--- WebAuthorizationTest/PageObjects/SearchPageObject.cs
+++ WebAuthorizationTest/PageObjects/SearchPageObject.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 namespace WebAuthorizationTest.PageObjects
 {
     public class SearchPageObject
     {
-        private IWebDriver _webDriver;
+        private readonly IWebDriver _webDriver;
+        private readonly WebDriverWait _wait;
 
         private readonly By _searchInput = By.Name("keyword");
         private readonly By _priceFromInput = By.XPath("//input[@aria-label='от']");
@@ -15,22 +17,29 @@ namespace WebAuthorizationTest.PageObjects
         public SearchPageObject(IWebDriver webDriver)
         {
             _webDriver = webDriver;
+            _wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(6));
         }
 
         public void Search(string text)
         {
-            _webDriver.FindElement(_searchInput).SendKeys(text);
+            IWebElement searchField = _wait.Until<IWebElement>(webDriver => _webDriver.FindElement(_searchInput));
+            searchField.SendKeys(text);
+            searchField.SendKeys(Keys.Enter);
         }
 
         public void InStock()
         {
-            _webDriver.FindElement(_inStockCheckBox).Click();
+            IWebElement inStockCheckBox = _wait.Until<IWebElement>(webDriver => _webDriver.FindElement(_inStockCheckBox));
+            inStockCheckBox.Click();
         }
 
         public void Price(int from, int to)
         {
-            _webDriver.FindElement(_priceFromInput).SendKeys(from.ToString());
-            _webDriver.FindElement(_priceToInput).SendKeys(to.ToString());
+            IWebElement priceFromField = _wait.Until<IWebElement>(webDriver => _webDriver.FindElement(_priceFromInput));
+            IWebElement priceToInput = _webDriver.FindElement(_priceToInput);
+            priceFromField.SendKeys(from.ToString());
+            priceToInput.SendKeys(to.ToString());
+            priceToInput.SendKeys(Keys.Enter);
         }
 
 
